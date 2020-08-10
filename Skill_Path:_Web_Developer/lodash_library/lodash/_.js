98a0f7825d97eae28d2931b412794ed028a47aec
James Barrington <james.barrington1985@gmail.com>
2020-08-10 13:43:00 +0100

    Implmented _.clamp()

---
+++ Skill_Path:_Web_Developer/lodash_library/lodash/_.js
@@ -0,0 +1,12 @@
+// _.clamp()
+
+const _ = {
+  clamp (number, lower, upper) {
+    const lowerClampedValue = Math.max(number, lower);
+    const clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  }
+};
+
+// Do not write or modify code below this line.
+module.exports = _;
