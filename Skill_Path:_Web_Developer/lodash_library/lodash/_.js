c363b5a34d34599332aff8d86dda9bc3b364b134
James Barrington <james.barrington1985@gmail.com>
2020-08-11 16:05:22 +0100

    Completed inRange()

--- Skill_Path:_Web_Developer/lodash_library/lodash/_.js
+++ Skill_Path:_Web_Developer/lodash_library/lodash/_.js
@@ -1,12 +1,29 @@
-// _.clamp()
+// lodash project
 
 const _ = {
-  clamp (number, lower, upper) {
+
+  clamp(number, lower, upper) {
     const lowerClampedValue = Math.max(number, lower);
     const clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
+  },
+
+  inRange(number, start, end) {
+    if ( end === undefined ) {
+      end = start
+      start = 0
+    }
+    if ( start > end ) {
+      const temp = end
+      end = start
+      start = temp
+    }
+    const isInRange = start <= number && number < end; 
+    return isInRange
   }
+
 };
+
 
 // Do not write or modify code below this line.
 module.exports = _;
